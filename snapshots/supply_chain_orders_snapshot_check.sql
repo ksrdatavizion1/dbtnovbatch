b8acdbc39a5b57b44586584af2531aed6af7c2bf
ksr datavizion <sehin69334@indevgo.com>
2026-03-23 02:38:28 +0000

    all chabges

---
+++ snapshots/supply_chain_orders_snapshot_check.sql
@@ -0,0 +1,15 @@
+{% snapshot supply_chain_scd2_table2 %}
+    {{
+        config(
+            target_schema='SILVER_SCHEMA',
+            target_database='DBT_LABS',
+            unique_key='ORDER_ID',
+            strategy='timestamp',
+            updated_at='UPDATED_AT',
+            hard_deletes = 'new_record',
+            snapshot_freshness = '2 days'
+        )
+    }}
+
+    select * from {{ source('raw_data', 'scd2') }}
+ {% endsnapshot %}
